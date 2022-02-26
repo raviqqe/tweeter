dcc774482dd53dc1e06ab1ed04596747386d3fc5
Yota Toyama <raviqqe@gmail.com>
2022-02-26 23:42:42 +0000

    Add more icons

--- tools/build.sh
+++ tools/build.sh
@@ -3,7 +3,19 @@
 set -e
 
 curl -fsSL https://dotown.maeda-design-room.net/wp-content/uploads/2022/01/thing_chick_01.png >images/original.png
-convert -size 32 images/original.png images/icon.png
+
+cp manifest.template.json manifest.json
+
+for size in 16 32 48 64 96 128; do
+  image=images/icon-$size.png
+
+  convert -size $size images/original.png $image
+
+  for key in .icons .action.default_icon; do
+    jq "$key[\"$size\"] = \"/$image\"" manifest.json >tmp.json
+    cp tmp.json manifest.json
+  done
+done
 
 npx web-ext build
 
