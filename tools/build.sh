de1c2d08fdc93804f76d5d6764147d560451e6c9
Yota Toyama <raviqqe@gmail.com>
2022-02-26 23:43:38 +0000

    Fix tmp directory

--- tools/build.sh
+++ tools/build.sh
@@ -5,6 +5,7 @@ set -e
 curl -fsSL https://dotown.maeda-design-room.net/wp-content/uploads/2022/01/thing_chick_01.png >images/original.png
 
 cp manifest.template.json manifest.json
+mkdir -p tmp
 
 for size in 16 32 48 64 96 128; do
   image=images/icon-$size.png
@@ -12,8 +13,8 @@ for size in 16 32 48 64 96 128; do
   convert -size $size images/original.png $image
 
   for key in .icons .action.default_icon; do
-    jq "$key[\"$size\"] = \"/$image\"" manifest.json >tmp.json
-    cp tmp.json manifest.json
+    jq "$key[\"$size\"] = \"/$image\"" manifest.json >tmp/manifest.json
+    cp tmp/manifest.json manifest.json
   done
 done
 
