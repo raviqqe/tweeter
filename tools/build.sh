7d11ed8719b347d87137a390dec5a281d3efb6c8
Yota Toyama <raviqqe@gmail.com>
2022-02-26 15:46:52 -0800

    Add more icons (#6)
    * Add more icons
    
    * Fix tmp directory
    
    * Overwrite destination

--- tools/build.sh
+++ tools/build.sh
@@ -3,9 +3,22 @@
 set -e
 
 curl -fsSL https://dotown.maeda-design-room.net/wp-content/uploads/2022/01/thing_chick_01.png >images/original.png
-convert -size 32 images/original.png images/icon.png
 
-npx web-ext build
+cp manifest.template.json manifest.json
+mkdir -p tmp
+
+for size in 16 32 48 64 96 128; do
+  image=images/icon-$size.png
+
+  convert -size $size images/original.png $image
+
+  for key in .icons .action.default_icon; do
+    jq "$key[\"$size\"] = \"/$image\"" manifest.json >tmp/manifest.json
+    cp tmp/manifest.json manifest.json
+  done
+done
+
+npx web-ext build --overwrite-dest
 
 for file in web-ext-artifacts/*.zip; do
   cp $file ${file%.zip}.crx
