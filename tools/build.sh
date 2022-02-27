a38c12d94a31b5d770571fcb05e22a5e166c1d3d
Yota Toyama <raviqqe@gmail.com>
2022-02-27 16:31:12 +0000

    Fix again

--- tools/build.sh
+++ tools/build.sh
@@ -20,7 +20,7 @@ for size in 16 32 48 64 96 128; do
     images/original.png \
     $image
 
-  for key in .icons .browserAction.default_icon; do
+  for key in .icons .browser_action.default_icon; do
     jq "$key[\"$size\"] = \"/$image\"" manifest.json >tmp/manifest.json
     cp tmp/manifest.json manifest.json
   done
