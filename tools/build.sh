511736bfbe4ce5a9bb8ad58783196335bd5d0a83
Yota Toyama <raviqqe@gmail.com>
2022-02-27 16:45:17 +0000

    Add polyfill

--- tools/build.sh
+++ tools/build.sh
@@ -26,4 +26,7 @@ for size in 16 32 48 64 96 128; do
   done
 done
 
+npm install
+cp node_modules/webextension-polyfill/dist/browser-polyfill.min.js src/polyfill.js
+
 npx web-ext build --overwrite-dest $web_ext_options
